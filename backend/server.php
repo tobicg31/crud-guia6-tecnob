7258bf51f828d5df2748ac95ef4b2eacec743478
tobicg31 <82407605+tobicg31@users.noreply.github.com>
2025-05-16 15:54:16 -0300

    Update server.php

--- backend/server.php
+++ backend/server.php
@@ -16,6 +16,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
 require_once("./routes/studentsRoutes.php"); //incluye el archivo que define las rutas o la logica
 /*
-require_once(""); si creo un nuevo archivo lo llamo aca
+require_once("routes/nuevomoduloRoutes.php"); si creo un nuevo archivo lo llamo aca
 */
 ?>
